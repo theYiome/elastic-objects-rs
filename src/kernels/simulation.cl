08cbf7602936180ac224d93747edba16433d49a5
Kamil Pasterczyk <yiome@outlook.com>
2022-05-14 00:58:14 +0200

    opencl almost works

--- src/kernels/simulation.cl
+++ src/kernels/simulation.cl
@@ -8,6 +8,13 @@
     #define KERNEL __kernel
 #endif
 
+
+struct Connection {
+    ulong j;
+    float dx;
+    float v0;
+};
+
 struct Node {
     float2 position;
     float2 velocity;
@@ -19,13 +26,85 @@ struct Node {
     bool is_boundary;
 };
 
-KERNEL void main(uint node_count, GLOBAL struct Node *nodes, uint collisions_count, GLOBAL ulong *collisions, GLOBAL ulong *collisions_index, uint dt_div, GLOBAL float2 *result) {
-    size_t index = get_global_id(0);
+const float COLLISION_V0 = 10.0f;
+const float COLLISION_DX = 0.015f;
 
-    float dt = 0.0;
-    if (dt_div != 0)
-        dt = 1.0 / dt_div;
+const float WALL_V0 = 200.f;
+const float WALL_DX = 0.05f;
 
-    float v = 10000000.0f * dt;
-    result[index] = (float2)(2.1f * v, 3.2f * v);
+KERNEL void main(
+    const uint node_count,
+    const GLOBAL struct Node * const nodes, 
+    const GLOBAL ulong * const collisions_index, 
+    const GLOBAL ulong * const collisions, 
+    const GLOBAL ulong * const connections_index,
+    const GLOBAL struct Connection * const connections,
+    GLOBAL float2 *result
+) {
+
+    size_t i = get_global_id(0);
+
+    if (i < node_count) {
+
+
+        float2 acceleration = (0.0f, 0.0f);
+
+        // Collisions
+        {
+            ulong collisions_index_start = 0;
+            if (i > 0)
+                collisions_index_start = collisions_index[i - 1];
+
+            const ulong collisions_index_end = collisions_index[i];
+
+
+            for (ulong c_i = collisions_index_start; c_i < collisions_index_end; c_i++) {
+                const ulong j = collisions[c_i];
+
+                float2 dir = nodes[j].position - nodes[i].position;
+                float l = length(dir);
+                float c = pown(COLLISION_DX / l, 13);
+                acceleration -= (normalize(dir) * 3.0f * (COLLISION_V0 / COLLISION_DX) * c);
+            }
+        }
+
+
+        // Connections
+        {
+            ulong connections_index_start = 0;
+            if (i > 0)
+                connections_index_start = connections_index[i - 1];
+            
+            const ulong connections_index_end = connections_index[i];
+
+            for (ulong c_i = connections_index_start; c_i < connections_index_end; c_i++) {
+                const ulong j = connections[c_i].j;
+                const float dx = connections[c_i].dx;
+                const float v0 = connections[c_i].v0;
+
+                float2 dir = nodes[j].position - nodes[i].position;
+                float l = length(dir);
+                float c = pown(dx / l, 7) - pown(dx / l, 13);
+                acceleration += (normalize(dir) * 3.0f * (v0 / dx) * c);
+            }
+        }
+
+        // Wall
+        {
+            float2 dir = (nodes[i].position.x, -1.0f) - nodes[i].position;
+            float l = length(dir);
+            float c = pown(WALL_DX / l, 13);
+            acceleration -= (normalize(dir) * 3.0f * (WALL_V0 / WALL_DX) * c);
+        }
+
+        // Sum up acceleration
+        result[i] += acceleration / nodes[i].mass;
+
+        // Drag
+        result[i] -= nodes[i].velocity * length(nodes[i].velocity) * nodes[i].drag;
+
+        // Gravity
+        result[i].y -= 9.81f;
+    }
+
 }
--- src/simulation/gpu.rs
+++ src/simulation/gpu.rs
@@ -2,19 +2,9 @@
 pub mod gpu {
     use glam::Vec2;
     
-    use rust_gpu_tools::{cuda, opencl, program_closures, Device, GPUError, Program};
+    use rust_gpu_tools::{opencl, program_closures, GPUError, Program};
 
-    use crate::simulation::{node::{Node, self}, cpu::{start_integrate_velocity_verlet, end_integrate_velocity_verlet}};
-    
-    // fn cuda(device: &Device) -> Program {
-    //     // The kernel was compiled with:
-    //     // nvcc -fatbin -gencode=arch=compute_52,code=sm_52 -gencode=arch=compute_60,code=sm_60 -gencode=arch=compute_61,code=sm_61 -gencode=arch=compute_70,code=sm_70 -gencode=arch=compute_75,code=sm_75 -gencode=arch=compute_75,code=compute_75 --x cu add.cl
-    //     let cuda_kernel = include_bytes!("../kernels/simulation.fatbin");
-    //     let cuda_device = device.cuda_device().unwrap();
-    //     let cuda_program = cuda::Program::from_bytes(cuda_device, cuda_kernel).unwrap();
-    
-    //     Program::Cuda(cuda_program)
-    // }
+    use crate::simulation::{node::Node, cpu::{start_integrate_velocity_verlet, end_integrate_velocity_verlet}};
     
     pub fn create_opencl_program() -> Program {
         let device = *rust_gpu_tools::Device::all().first().unwrap();
@@ -22,6 +12,20 @@ pub mod gpu {
         let opencl_device = device.opencl_device().unwrap();
         let opencl_program = opencl::Program::from_opencl(opencl_device, opencl_kernel).unwrap();
         Program::Opencl(opencl_program)
+    }
+
+    pub fn flat_with_indexes<T: Copy>(nested_slice: &[Vec<T>]) -> (Vec<T>, Vec<usize>) {
+        let flat: Vec<T> = nested_slice.iter().flatten().copied().collect();
+        let mut indexes: Vec<usize> = Vec::with_capacity(nested_slice.len());
+
+        let mut current_index = 0;
+        nested_slice.iter().enumerate().for_each(|(i, _)| {
+            let new_index = current_index + nested_slice[i].len();
+            indexes.push(new_index);
+            current_index = new_index;
+        });
+
+        (flat, indexes)
     }
     
     pub fn simulate_opencl(
@@ -35,8 +39,8 @@ pub mod gpu {
         start_integrate_velocity_verlet(dt, nodes);
 
         {
-            let flat_connections: Vec<&(usize, f32, f32)> = connections_structure.iter().flatten().collect();
-            let flat_collisions: Vec<&usize> = collisions_structure.iter().flatten().collect();
+            // let flat_connections: Vec<&(usize, f32, f32)> = connections_structure.iter().flatten().collect();
+            let flat_collisions: Vec<usize> = collisions_structure.iter().flatten().copied().collect();
             let mut collisions_indexes: Vec<usize> = vec![0; nodes.len()];
             // println!("{}", collisions_indexes.len());
     
@@ -46,16 +50,44 @@ pub mod gpu {
                 collisions_indexes[i] = new_index;
                 current_index = new_index;
             });
+
+            //---------------------------------------------------------------------------------------------------------------------
+
+            let flat_connections: Vec<(usize, f32, f32)> = connections_structure.iter().flatten().copied().collect();
+            let mut connections_indexes: Vec<usize> = vec![0; nodes.len()];
+            // println!("{}", collisions_indexes.len());
+    
+            let mut current_index = 0;
+            nodes.iter().enumerate().for_each(|(i, _)| {
+                let new_index = current_index + connections_structure[i].len();
+                connections_indexes[i] = new_index;
+                current_index = new_index;
+            });
+
+            // print collisions_indexes
+            // collisions_indexes.iter().for_each(|i| {
+            //     print!("{} ", i);
+            // });
+            
+            // print flat_collisions
+            // flat_collisions.iter().for_each(|i| {
+            //     print!("{} ", i);
+            // });
+            // println!("\n{} {}", collisions_indexes.len(), flat_collisions.len());
+
                 
-            let closures = program_closures!(|program, _args| -> Result<Vec<Vec2>, GPUError> {
+            let fun = program_closures!(|program, _args| -> Result<Vec<Vec2>, GPUError> {
                 // Make sure the input data has the same length.
-                let dt_div = if dt != 0.0 { (1.0 / dt) as u32 } else { 0 };
+                let dt_div: u32 = if dt != 0.0 { (1.0 / dt) as u32 } else { 0 };
                 // println!("{}", dt_div);
                 
                 // Copy the data to the GPU.
                 let node_buffer = program.create_buffer_from_slice(&nodes)?;
-                let collision_structure_buffer = program.create_buffer_from_slice(&flat_collisions)?;
                 let collision_indexes_buffer = program.create_buffer_from_slice(&collisions_indexes)?;
+                let collision_structure_buffer = program.create_buffer_from_slice(&flat_collisions)?;
+
+                let connection_indexes_buffer = program.create_buffer_from_slice(&connections_indexes)?;
+                let connection_structure_buffer = program.create_buffer_from_slice(&flat_connections)?;
     
                 let mut result: Vec<Vec2> = vec![Vec2::new(0.0, 0.0); nodes.len()];
                 let result_buffer = program.create_buffer_from_slice(&result)?;
@@ -69,10 +101,10 @@ pub mod gpu {
                 kernel
                     .arg(&(nodes.len() as u32))
                     .arg(&node_buffer)
-                    .arg(&(flat_collisions.len() as u32))
-                    .arg(&collision_structure_buffer)
                     .arg(&collision_indexes_buffer)
-                    .arg(&dt_div)
+                    .arg(&collision_structure_buffer)
+                    .arg(&connection_indexes_buffer)
+                    .arg(&connection_structure_buffer)
                     .arg(&result_buffer)
                     .run()?;
         
@@ -82,7 +114,7 @@ pub mod gpu {
                 Ok(result)
             });
         
-            let result = program.run(closures, ()).unwrap();
+            let result = program.run(fun, ()).unwrap();
     
             nodes.iter_mut().enumerate().for_each(|(i, n)| {
                 // println!("{} {} {}", i, result[i].x, result[i].y);
